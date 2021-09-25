e14b1b0d185f179a34068bd96d1b2f874d837d15
18072741958 <960302109@qq.com>
2021-09-25 17:06:15 +0800

    第六题作业

--- week02/06/httpclient/src/main/java/com/example/HttpClientUtil.java
+++ week02/06/httpclient/src/main/java/com/example/HttpClientUtil.java
@@ -20,47 +20,53 @@ import java.util.List;
 
 public class HttpClientUtil {
 
-    static CloseableHttpClient  httpClient = HttpClients.createDefault();
+    static CloseableHttpClient httpClient = HttpClients.createDefault();
     static final String BAIDU_URL = "http://www.baidu.com";
     static final String LOCALHOST_URL = "http://localhost:8801/";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         get(LOCALHOST_URL);
     }
 
-    public static void get(String url){
+    public static void get(String url) {
         HttpGet get = new HttpGet(url);
         try {
             HttpResponse response = httpClient.execute(get);
-            if(200 == response.getStatusLine().getStatusCode()){
+            if (200 == response.getStatusLine().getStatusCode()) {
                 HttpEntity httpEntity = response.getEntity();
-                String message = EntityUtils.toString(httpEntity,"utf-8");
+                String message = EntityUtils.toString(httpEntity, "utf-8");
                 System.out.println(message);
             } else {
                 System.out.println("error" + response.getStatusLine().getStatusCode());
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public static void post(String url,List<NameValuePair> nvps){
+    public static void post(String url, List<NameValuePair> nvps) {
         HttpPost post = new HttpPost(url);
         CloseableHttpResponse response = null;
         try {
-            UrlEncodedFormEntity uef = new UrlEncodedFormEntity(nvps,"utf-8");
+            UrlEncodedFormEntity uef = new UrlEncodedFormEntity(nvps, "utf-8");
             post.setEntity(uef);
             response = httpClient.execute(post);
             HttpEntity entity = response.getEntity();
-            String message = EntityUtils.toString(entity,"utf-8");
+            String message = EntityUtils.toString(entity, "utf-8");
             Header[] headers = response.getAllHeaders();
-            if(message != null){
+            if (message != null) {
                 System.out.println("headers" + headers);
                 System.out.println(message);
             }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
